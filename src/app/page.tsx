e0869d881c61744138784983ca4a3fdad353136a
Lavesh Nama Kamalesh <laveshnk@gmail.com>
2023-12-30 13:35:50 -0500

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import "./globals.css";
+
+export default function HomePage() {
+  return (
+    <div>
+      <div className="bg-gradient-to-b from-blue-600 h-screen to-purple-600">
+        <div className="flex items-center justify-center">
+          <h1 className="bg-text1 mt-16">
+            GitGPT
+          </h1>
+        </div>
+                                                
+      </div>
+    </div>
+  );
+}
