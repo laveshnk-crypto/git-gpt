94feb03cb113352648d65e14465a0d372eaf5aed
Lavesh Nama Kamalesh <laveshnk@gmail.com>
2023-12-30 22:24:47 -0500

    Added functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import "./globals.css";
+import ChatComponent from "./components/chatComponent";
 
 export default function HomePage() {
+
+// ChatComponent
+
   return (
     <div>
       <div className="bg-gradient-to-b from-blue-600 h-screen to-purple-600">
@@ -9,7 +13,11 @@ export default function HomePage() {
             GitGPT
           </h1>
         </div>
-                                                
+        <div className="flex items-center justify-center">
+         <div className="bg-slate-950 p-3 h-[800px] w-[800px] overflow-y:auto rounded-md text-white mt-10">
+            <ChatComponent/>
+         </div>  
+        </div>
       </div>
     </div>
   );
